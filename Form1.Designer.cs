40fe4099d665b9b9417e9352512a935b89c295ca
Joshua Figueiredo <339727042@GoTVDSB.ca>
2024-10-23 09:40:12 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace topic_10_assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSun = new System.Windows.Forms.Button();
+            this.btnMercury = new System.Windows.Forms.Button();
+            this.btnVenus = new System.Windows.Forms.Button();
+            this.btnEarth = new System.Windows.Forms.Button();
+            this.btnMars = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnNeptune = new System.Windows.Forms.Button();
+            this.btnUranus = new System.Windows.Forms.Button();
+            this.btnSaturn = new System.Windows.Forms.Button();
+            this.btnJupiter = new System.Windows.Forms.Button();
+            this.imgPlanet = new System.Windows.Forms.PictureBox();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlanet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSun
+            // 
+            this.btnSun.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSun.Location = new System.Drawing.Point(12, 36);
+            this.btnSun.Name = "btnSun";
+            this.btnSun.Size = new System.Drawing.Size(126, 64);
+            this.btnSun.TabIndex = 0;
+            this.btnSun.Text = "The Sun";
+            this.btnSun.UseVisualStyleBackColor = true;
+            // 
+            // btnMercury
+            // 
+            this.btnMercury.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMercury.Location = new System.Drawing.Point(12, 106);
+            this.btnMercury.Name = "btnMercury";
+            this.btnMercury.Size = new System.Drawing.Size(126, 64);
+            this.btnMercury.TabIndex = 1;
+            this.btnMercury.Text = "Mercury";
+            this.btnMercury.UseVisualStyleBackColor = true;
+            // 
+            // btnVenus
+            // 
+            this.btnVenus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVenus.Location = new System.Drawing.Point(12, 176);
+            this.btnVenus.Name = "btnVenus";
+            this.btnVenus.Size = new System.Drawing.Size(126, 64);
+            this.btnVenus.TabIndex = 2;
+            this.btnVenus.Text = "Venus";
+            this.btnVenus.UseVisualStyleBackColor = true;
+            // 
+            // btnEarth
+            // 
+            this.btnEarth.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEarth.Location = new System.Drawing.Point(12, 246);
+            this.btnEarth.Name = "btnEarth";
+            this.btnEarth.Size = new System.Drawing.Size(126, 64);
+            this.btnEarth.TabIndex = 3;
+            this.btnEarth.Text = "Earth";
+            this.btnEarth.UseVisualStyleBackColor = true;
+            // 
+            // btnMars
+            // 
+            this.btnMars.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMars.Location = new System.Drawing.Point(12, 316);
+            this.btnMars.Name = "btnMars";
+            this.btnMars.Size = new System.Drawing.Size(126, 64);
+            this.btnMars.TabIndex = 4;
+            this.btnMars.Text = "Mars";
+            this.btnMars.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Location = new System.Drawing.Point(662, 316);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(126, 64);
+            this.btnQuit.TabIndex = 9;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // btnNeptune
+            // 
+            this.btnNeptune.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNeptune.Location = new System.Drawing.Point(662, 246);
+            this.btnNeptune.Name = "btnNeptune";
+            this.btnNeptune.Size = new System.Drawing.Size(126, 64);
+            this.btnNeptune.TabIndex = 8;
+            this.btnNeptune.Text = "Neptune";
+            this.btnNeptune.UseVisualStyleBackColor = true;
+            // 
+            // btnUranus
+            // 
+            this.btnUranus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUranus.Location = new System.Drawing.Point(662, 176);
+            this.btnUranus.Name = "btnUranus";
+            this.btnUranus.Size = new System.Drawing.Size(126, 64);
+            this.btnUranus.TabIndex = 7;
+            this.btnUranus.Text = "Uranus";
+            this.btnUranus.UseVisualStyleBackColor = true;
+            // 
+            // btnSaturn
+            // 
+            this.btnSaturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaturn.Location = new System.Drawing.Point(662, 106);
+            this.btnSaturn.Name = "btnSaturn";
+            this.btnSaturn.Size = new System.Drawing.Size(126, 64);
+            this.btnSaturn.TabIndex = 6;
+            this.btnSaturn.Text = "Saturn";
+            this.btnSaturn.UseVisualStyleBackColor = true;
+            // 
+            // btnJupiter
+            // 
+            this.btnJupiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJupiter.Location = new System.Drawing.Point(662, 36);
+            this.btnJupiter.Name = "btnJupiter";
+            this.btnJupiter.Size = new System.Drawing.Size(126, 64);
+            this.btnJupiter.TabIndex = 5;
+            this.btnJupiter.Text = "Jupiter";
+            this.btnJupiter.UseVisualStyleBackColor = true;
+            // 
+            // imgPlanet
+            // 
+            this.imgPlanet.Location = new System.Drawing.Point(252, 36);
+            this.imgPlanet.Name = "imgPlanet";
+            this.imgPlanet.Size = new System.Drawing.Size(297, 204);
+            this.imgPlanet.TabIndex = 10;
+            this.imgPlanet.TabStop = false;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(293, 9);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(217, 20);
+            this.lblStart.TabIndex = 11;
+            this.lblStart.Text = "Press a Button to get started!";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(144, 267);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(512, 156);
+            this.lblInfo.TabIndex = 12;
+            this.lblInfo.Text = resources.GetString("lblInfo.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.imgPlanet);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnNeptune);
+            this.Controls.Add(this.btnUranus);
+            this.Controls.Add(this.btnSaturn);
+            this.Controls.Add(this.btnJupiter);
+            this.Controls.Add(this.btnMars);
+            this.Controls.Add(this.btnEarth);
+            this.Controls.Add(this.btnVenus);
+            this.Controls.Add(this.btnMercury);
+            this.Controls.Add(this.btnSun);
+            this.Name = "Form1";
+            this.Text = "Planet Information";
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlanet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSun;
+        private System.Windows.Forms.Button btnMercury;
+        private System.Windows.Forms.Button btnVenus;
+        private System.Windows.Forms.Button btnEarth;
+        private System.Windows.Forms.Button btnMars;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnNeptune;
+        private System.Windows.Forms.Button btnUranus;
+        private System.Windows.Forms.Button btnSaturn;
+        private System.Windows.Forms.Button btnJupiter;
+        private System.Windows.Forms.PictureBox imgPlanet;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
